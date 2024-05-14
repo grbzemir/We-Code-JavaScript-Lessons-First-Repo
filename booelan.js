6b40f6448c159ed6873b9ec5d5bf569135da6a29
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-14 20:28:33 +0300

    first commit

---
+++ booelan.js
@@ -0,0 +1,30 @@
+// ********** Boolean Veri Turu ile Calismak ********** 
+
+// 0 ve 1'i Anlamak ->
+let isActive = false // 0
+isActive = true // 1
+console.log(isActive)
+
+
+let userName;
+let isUserName = Boolean(userName)
+console.log(isUserName)
+
+//Boolen içinde veri varsa true veri yoksa false olarak gösterir
+Boolean("14") // true
+Boolean("11") // true
+Boolean("0") // true
+Boolean("") // false
+
+userName = "user"
+
+console.log("User Name:", Boolean(userName))
+
+// 0 ,-0, null, false, NaN, undefined, ("") ->
+Boolean(0) //  false
+Boolean(-0) // false
+Boolean(-0.1) // true
+Boolean(0 === 0) // true
+
+// Not: Karar Yapilari Kisminda Tekrar Anlatilacak
+Boolean(userName.length > 0) // true
