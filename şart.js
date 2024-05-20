4f7f6f411501535c86d37f1ddad227c5b0266cc4
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-20 18:55:56 +0300

    first commit

---
+++ şart.js
@@ -0,0 +1,12 @@
+let username = prompt("Kullanici adinizi giriniz:")
+
+if (username.length > 15) {
+
+    console.log(`Kullanici bilginiz: ${username}`);
+
+}
+else {
+
+    console.log("bilgi yok");
+
+}
