b7566a0cd88540f22454b828d7b4a08f25ba412d
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-22 23:08:52 +0300

    first commit

--- formandsubmit.js
+++ formandsubmit.js
@@ -7,6 +7,9 @@ formDOM.addEventListener('submit', formSubmit)
 function formSubmit(event) {
     event.preventDefault()
     console.log("islem gerceklesti")
+    let scoreInputDOM = document.querySelector('#score')
+    console.log(scoreInputDOM.value)
+    localStorage.setItem('score', scoreInputDOM.value)
 }
 
 //form submit oldugunda sadece veri değişir
