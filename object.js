8cf14e7320d6d22a5c903009f09f7f6de37f83f2
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-25 14:28:59 +0300

    first commit

---
+++ object.js
@@ -0,0 +1,27 @@
+
+let arrayObj = [1, 2, 3] // Dizi seklinde object tanımlandı
+let object1 = { obj: 1 }
+
+//çalışma zamanı türünün objectexpression ile typename uyumlu olup olmadığını belirler
+
+console.log("arrayObj", typeof { arrayObj })
+console.log("object1", typeof { object1 })
+
+let item1 = new Object()
+let item2 = {}
+
+console.log("item1", typeof (item1))
+console.log("item2", typeof (item2))
+
+//object olusturmak
+
+
+let item3 = {}
+let item4 = new Object()
+
+console.log("object1", object1)
+
+
+
+
+
